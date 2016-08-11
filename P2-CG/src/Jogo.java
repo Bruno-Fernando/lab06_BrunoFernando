c6d591c8055a7e63c5ee31fb91263509e9247adf
BRUNO FERNANDO DA SILVA 115211111 <brunofs@lcc2-28.lcc.ufcg.edu.br>
2016-08-11 11:47:59 -0300

    Adcionando primeiro dia do lab

---
+++ P2-CG/src/Jogo.java
@@ -0,0 +1,18 @@
+
+public class Jogo {
+	String nome;
+	double preco;
+	int maxScore;
+	int jogadas;
+	int zeradas;
+	
+	public Jogo(String nome,double preco){
+		this.nome = nome;
+		this.preco = preco;
+		this.maxScore = 0;
+		this.jogadas = 0;
+		this.zeradas = 0;
+	}
+	
+
+}
